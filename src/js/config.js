8634b8e41ba3571031fbe996b098f62a398e136c
Jaffaarr <jaffarson1234@gmil.com>
2023-04-16 15:30:20 +0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ef93fdbf-1244-4a9d-ba3c-a9fbec1ed21d';
+export const MODAL_CLOSE_SEC = 2.5;
